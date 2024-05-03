73657addb3ffa0ea5aa00219945d3ffa737e16e3
Priyanshu085 <abpriyanshu007@gmail.com>
2024-05-03 16:55:44 +0530

    Initial commit

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Personal Room
+    </div>
+  )
+}
+
+export default page
---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from './ui/button'
+
+const Navbar = () => {
+  return (
+    <div className='flex'>
+      {/* <div className='text-white text-4xl m-2'>
+        DEVTALKS
+      </div> */}
+      {/* <div className="items-end float-end text-right object-right text-white text-xl p-4'>
+        <Button>
+          <Link href='/login'>Login</Link>
+        </Button>
+      </div> */}
+
+    </div>
+  )
+}
+
+export default Navbar
