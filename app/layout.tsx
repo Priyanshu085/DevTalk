1521e07be7b0876a1ac4598cba05bfda67614e08
Priyanshu <abpriyanshu007@gmail.com>
2024-05-10 05:05:06 +0530

    chore: Add @vercel/analytics package

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
@@ -40,6 +41,7 @@ export default function RootLayout({
         <body className={`${inter.className} bg-dark-2`}>
           <Toaster />
           {children}
+          <Analytics/> 
         </body>
       </ClerkProvider>
     </html>
